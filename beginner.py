115b936f5a6ae5c3a2dbdf446ca4ecfc21b7fec8
raddan1674 <tanrajtoor1674@gmail.com>
2025-08-10 08:28:44 -0700

    Add files via upload

---
+++ beginner.py
@@ -0,0 +1,17 @@
+'''
+f1=int(input('enter the first number: '))
+f2=int(input('enter the second number: '))
+addition= f1 + f2
+subtraction= f1 - f2
+multiplication= f1 * f2
+division= f1 / f2
+print('addition =',addition)
+print('subtraction =',subtraction)
+print('multiplication =',multiplication)
+print('division =',division)
+'''
+
+firstname=input('enter your first name: ')
+lastname=input('enter your last name: ')
+fullname= firstname + ' ' + lastname
+print('Hello,' ,fullname + '! Welcome to the Python program.')
